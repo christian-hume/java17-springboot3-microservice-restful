97c1924a58eacb8f236ec8b2021acebb60b9fff2
christian-hume <christian-hume@outlook.com>
2025-09-30 10:26:36 -0300

    fix: Content was decoded with errors (using 'UTF-8' charset)
    Signed-off-by: Christian Hume <christian-hume@outlook.com>

--- src/test/java/com/java17/springboot3/microservice/restful/ApplicationTests.java
+++ src/test/java/com/java17/springboot3/microservice/restful/ApplicationTests.java
@@ -3,7 +3,7 @@ package com.java17.springboot3.microservice.restful;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+ @SpringBootTest
 class ApplicationTests {
 
 	@Test
